9128938a2ba81318f215ee35671a0f2540716a1b
tdhoan526 <tdhoan526@gmail.com>
2022-08-14 08:57:08 +0700

    First release of Hello World!

---
+++ keepalived/checkapi_server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+errorExit() {
+    echo "*** $*" 1>&2
+    exit 1
+}
+curl --silent --max-time 2 --insecure https://localhost:6443/ -o /dev/null || errorExit "Error GET https://localhost:6443/"
+if ip addr | grep -q "{{ virtip }}"; then
+    curl --silent --max-time 2 --insecure https://"{{ virtip }}":6443/ -o /dev/null || errorExit "Error GET https://"{{  virtip  }}":6443/"
+fi
